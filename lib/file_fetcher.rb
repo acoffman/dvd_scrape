d1f0e99d249235c81067b4386568a056ebf8f8da
Adam Coffman <coffman.adam@gmail.com>
2009-01-20 14:41:23 -0600

    changed the file fetcher, still need to comment and clean it up, but it works now

--- lib/file_fetcher.rb
+++ lib/file_fetcher.rb
@@ -1,17 +1,55 @@
-require 'FileUtils'
+require 'rubygems'
+require 'net/http'
+require 'zip/zipfilesystem'
 
-class FileFetcher
+@address = ARGV[0]
+#default value unless alternate is specified on the command line
+@address ||= 'http://www.hometheaterinfo.com/download/dvd_csv.zip'
 
-	def initialize(url)
-		if File.exists?(url)
-			@url = url
-		else
-			raise "There is not a file at the URL you specified"
-		end
+def download_file
+	#split file path on / characters and check to see if http:// was entered
+	url_array = @address.split('/')
+	if url_array[0].eql? "http:" 
+  	domain = url_array[2]
+  	start_count = 3
+	else 
+  	domain = url_array[0]
+  	start_count = 1
 	end
-
-	attr_accessor :url
-
+ 
+	#now that we have the domain, rebuild the path to the file
+	path = ""
+	start_count.upto(url_array.length() -1) do |x|
+  	path << '/'
+  	path << url_array[x]
+	end
+	puts "Downloading the file..."
+	Net::HTTP.start(domain) do |http|
+  	resp = http.get(path)
+  	open(@address.split("/").last, "wb") do |file|
+  	  file.write(resp.body)
+  	end
+	end
+	puts "Download complete"
 end
 
-test = FileFetcher.new "http://www.hometheaterinfo.com/download/dvd_csv.zip"
+def unzip(source_file, target_dir)
+	Dir.mkdir target_dir if not File.exists? target_dir
+	
+	Zip::ZipFile.open(source_file) do |zip|
+		curr_dir = zip.dir
+
+		curr_dir.entries('.').each do |file|
+			zip.extract(file, "#{target_dir}/#{file}")
+		end
+	end
+	rescue Zip::ZipDestinationFileExistsError => ex
+end
+
+def cleanup
+	FileUtils.rm(@address.split("/").last)
+end
+
+download_file
+unzip(@address.split("/").last, "../dvd")
+cleanup
