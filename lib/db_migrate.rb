06b3a42eb478f8e6032fa26e284bcb1453ded86e
Adam Coffman <coffman.adam@gmail.com>
2009-01-20 13:54:23 -0600

    added migration for db table

---
+++ lib/db_migrate.rb
@@ -0,0 +1,40 @@
+#This is only required to set up the initial dvds table in the 
+#dvd_parse database, it should only need to be run once
+
+require 'rubygems'
+require 'activerecord'
+
+#connect to the mysql database we're going to use
+ActiveRecord::Base.establish_connection({
+	:adapter => 'mysql',
+	:username => 'root',
+	:database => 'dvd_parse'
+})
+
+
+#initial migration to create our table
+class CreateDVDsTable < ActiveRecord::Migration
+
+	def self.up
+		create_table :dvds do |t|
+			t.string :title
+			t.string :studio
+			t.string :released
+			t.string :status
+			t.string :sound
+			t.string :versions
+			t.string :year
+			t.string :genre
+			t.string :aspect
+			t.string :upc
+			t.string :dvd_release
+		end
+	end
+
+	def self.down
+		drop_table :dvds
+	end
+
+end
+
+CreateDVDsTable.migrate(:up)
